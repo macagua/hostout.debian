ae7689f1de000c7d324a9b64ddc55a35f1c89568
Leonardo J .Caballero G <leonardocaballero@gmail.com>
2010-11-30 11:38:16 -0430

    Updated documentation and added new instructions of installation

--- hostout/debian/fabfile.py
+++ hostout/debian/fabfile.py
@@ -42,9 +42,11 @@ def bootstrap():
     print d
     api.run('uname -r')
 
-#    api.sudo('aptitude -y update')
-#    api.sudo('aptitude -y safe-upgrade ')
-    
+    # Update from sources list repositories
+    api.sudo('aptitude -y update')
+
+    # A safe upgrade of debian
+    api.sudo('aptitude -y safe-upgrade ')
     
     version = api.env['python-version']
     major = '.'.join(version.split('.')[:2])
@@ -54,18 +56,22 @@ def bootstrap():
     #contrib.files.append(apt_source, '/etc/apt/source.list', use_sudo=True)
     api.sudo('aptitude -yq install '
              'build-essential '
-#             'python%(major)s python%(major)s-dev '
-#             'python-libxml2 '
-#             'python-elementtree '
-#             'python-celementtree '
+             'python%(major)s python%(major)s-dev '
+             'python-libxml2 '
              'ncurses-dev '
              'libncurses5-dev '
-# needed for lxml on lucid
              'libz-dev '
              'libdb4.6 '
              'libxp-dev '
-             'libreadline5 '
+             'libssl-dev '
              'libreadline5-dev '
+             'readline-common '
+             'libxml2-dev '
+             'tar '
+             'unzip '
+             'bzip2 '
+             'wv '
+             'xpdf-utils '
              % locals())
 
     try:
@@ -80,17 +86,11 @@ def bootstrap():
 
     #api.sudo('aptitude -yq update; aptitude safe-upgrade; aptitude dist-upgrade')
 
-#    api.sudo('apt-get install python2.4=2.4.6-1ubuntu3.2.9.10.1 python2.4-dbg=2.4.6-1ubuntu3.2.9.10.1 \
-# python2.4-dev=2.4.6-1ubuntu3.2.9.10.1 python2.4-doc=2.4.6-1ubuntu3.2.9.10.1 \
-# python2.4-minimal=2.4.6-1ubuntu3.2.9.10.1')
-    #wget http://mirror.aarnet.edu.au/pub/ubuntu/archive/pool/main/p/python2.4/python2.4-minimal_2.4.6-1ubuntu3.2.9.10.1_i386.deb -O python2.4-minimal.deb
-    #wget http://mirror.aarnet.edu.au/pub/ubuntu/archive/pool/main/p/python2.4/python2.4_2.4.6-1ubuntu3.2.9.10.1_i386.deb -O python2.4.deb
-    #wget http://mirror.aarnet.edu.au/pub/ubuntu/archive/pool/main/p/python2.4/python2.4-dev_2.4.6-1ubuntu3.2.9.10.1_i386.deb -O python2.4-dev.deb
-    #sudo dpkg -i python2.4-minimal.deb python2.4.deb python2.4-dev.deb
-    #rm python2.4-minimal.deb python2.4.deb python2.4-dev.deb
-
     # python-profiler?
-    
+    api.sudo('aptitude -yq install python-profiler' % locals())
+
+    # Repository control version tools
+    api.sudo('aptitude -yq install subversion git-core' % locals())
 
     #ensure bootstrap files have correct owners
     hostout.setowners()
@@ -104,8 +104,19 @@ def predeploy():
         bootstrap()
     #bootstrap()
 
+
+def installPIP():
+    """ Install dependecies for PIP Tool on debian """
+    version = api.env['python-version']
+    major = '.'.join(version.split('.')[:2])
+    
+    #to install PIP tool
+    api.sudo('aptitude -yq install python-setuptools' % locals())
+    api.sudo('easy_install-%(major)s pip' % locals())
+
+
 def installPIL():
-    """ Install dependecies for PIL on ubuntu """
+    """ Install dependecies for PIL on debian """
     hostout = api.env.get('hostout')
 
     version = api.env['python-version']
@@ -138,6 +149,4 @@ def installPIL():
     #--- FREETYPE2 support ok
 
     #--------------------------------------------------------------------
-
-
 
